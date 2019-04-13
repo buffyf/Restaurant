a64ccedb0f594f8a61d9b96e6dcdb83268de80fa
Buffy Feinstein <buffyfeinstein@gmail.com>
2019-04-13 15:57:59 -0500

    Storyboard flow set-up

---
+++ Restaurant/MenuItemDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuItemDetailViewController.swift
+//  Restaurant
+//
+//  Created by Barbara Feinstein on 4/13/19.
+//  Copyright © 2019 Barbara Feinstein. All rights reserved.
+//
+
+import UIKit
+
+class MenuItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
